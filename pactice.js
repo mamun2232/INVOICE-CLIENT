21fc0d4c237b74610e0ad4c10ba7fca519f745e1
Your Name <you@example.com>
2022-02-15 21:14:19 +0600

    complect

--- pactice.js
+++ pactice.js
@@ -1,74 +1,83 @@
-
-const detailsButton = document.getElementById('detail-submit-btn')
-      detailsButton.addEventListener('click', function(){
-                const buyerDetails = document.getElementById('buyer-details-input')
-                document.getElementById('buyer-info').innerText = buyerDetails.value
-                buyerDetails.value = ''
-      })
-
-//       clsoe section
-
-const addProductBtn = document.getElementById('add-details-btn')
-addProductBtn.addEventListener('click', function(){
-       
-          const infoTable = document.getElementById('info-table')
-          // get input
-          const itemName = document.getElementById('item-name-input')
-          const itemPrice = document.getElementById('item-price-input')
-          const itemQuantity = document.getElementById('item-quantity-input')
-
-          // dainamic value add
-          /*  <tr>
-          <th >1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-      </tr>   */
-      const tutalPrice = parseInt(itemPrice.value) * parseInt(itemQuantity.value)
-
-          let tr = document.createElement('tr');
-          let th = document.createElement('th');
-          let td1 = document.createElement('td');
-          let td2 = document.createElement('td')
-          let td3 = document.createElement('td')
-          td3.classList.add = 'item-tutal'
-          // value add 
-          th.innerText = itemName.value;
-          td1.innerText = itemPrice.value;
-          td2.innerText = itemQuantity.value
-          td3.innerText = tutalPrice;
-
-          // value tansfram 
-          tr.appendChild(th);
-          tr.appendChild(td1);
-          tr.appendChild(td2);
-          tr.appendChild(td3); 
-          infoTable.appendChild(tr);
-          totalCalculation()
-        
+document.getElementById('detail-submit-btn').addEventListener('click', function(){
+          const buyerDetails = document.getElementById('buyer-details-input')
+          
+          const buyerInfo = document.getElementById('buyer-info')
+          buyerInfo.innerText = buyerDetails.value;
+          buyerDetails.value = '';
+    
 })
-function totalCalculation(){
-          const tutal = calculateSubTotal()
-          const subTutalDisplay = document.getElementById('sub-total')
-          subTutalDisplay.innerText = tutal;
- 
-
-          return subTutalDisplay.innerText
+// section tow start .................
+document.getElementById('add-details-btn').addEventListener('click', function(){
+          const tableInfo = document.getElementById('info-table')
+          const inputName =document.getElementById('item-name-input')
+          const inputPrice = document.getElementById('item-price-input')
+          const inputQuantity = document.getElementById('item-quantity-input')
           
 
-   
+
+          const tutalPrice = parseInt(inputPrice.value) * parseInt(inputQuantity.value)
+           const tr = document.createElement('tr')
+           const th = document.createElement('th');
+           const td = document.createElement('td');
+           const td1 = document.createElement('td')
+           const td2 = document.createElement('td')
+
+           th.innerText = inputName.value
+           td.innerText = inputPrice.value
+           td1.innerText = inputQuantity.value
+           td2.innerText = tutalPrice
+          // creat tag ................
+          td2.classList.add('item-tutal')
+
+           tr.appendChild(th)
+           tr.appendChild(td)
+           tr.appendChild(td1)
+           tr.appendChild(td2)
+
+           tableInfo.appendChild(tr)
+           inputName.value = ''
+           inputPrice.value = ''
+           inputQuantity.value = ''
+           tutalCalculation()
+         
+          
+})
+
+function tutalCalculation(){
+          const subtutal =  subCalculation();
+          const subtutalDispaly = document.getElementById('sub-total')
+          subtutalDispaly.innerText = subtutal;
+          
+          // tax ------
+          // const tax = document.getElementById('tax')
+          // const tutalTax =  subtutal * 0.2
+          // tax.innerText = tutalTax
+
+          // // grande tutal ----------
+          // const grande = document.getElementById('grand-total')
+          // const grandeTutal = subtutalDispaly + tax.innerText
+          const tax =  subtutal * 0.2
+
+          document.getElementById('tax').innerText = tax
+          document.getElementById('grand-total').innerText = subtutal + tax 
+          document.getElementById('grand-total-2').innerText = subtutal + tax
+          
+        
+          
+
+         
 }
 
-
-function calculateSubTotal(){
-          let subtutal = 0
-          const cost = document.getElementsByClassName('item-tutal')
-          for (let i = 0; i < cost.length; i++) {
+function subCalculation(){
+          let subtutal = 0;
+          const cost = document.getElementsByClassName('item-tutal');
+          for(let i = 0; i < cost.length ; i++){
                     const element = cost[i];
-                 let   price = parseInt(element.innerText)
-                    subtutal =  subtutal + price
-                    
-          }
-          return subtutal;
-}
+                    let price = parseInt(element.innerText)
+                    subtutal = subtutal + price
 
+          }
+
+          return subtutal;
+
+}
